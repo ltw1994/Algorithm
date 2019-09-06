18df6b3935253ecd633c681d39af4d687746e0da
임태완 <ltw2001@hanmail.net>
2019-09-06 19:47:48 +0900

    9/6_update

--- Baekjoon_Algorithm/Algorithm_cpp/삼성기출/16236_아기상어/16236_아기상어/main.cpp
+++ Baekjoon_Algorithm/Algorithm_cpp/삼성기출/16236_아기상어/16236_아기상어/main.cpp
@@ -13,26 +13,94 @@
 using namespace std ;
 
 struct now {
-    int xx, yy ;
-}
+    int yy, xx, size, eat, clk ;
+};
 
 queue<now> q ;
-int x ;
-int y ;
-int N ;
+int x, y, N, s, e, c ;
+int result ;
+bool Call_Mother = false ;
 int sea[21][21] ;
+bool visited[21][21] ;
 
-const int dx[] = {1, 0, -1, 0} , dy[] = {0, -1, 0, 1} ;
+const int dx[] = {0, -1, 1, 0} , dy[] = {-1, 0, 0, 1} ;
 
 void find_fish() {
-    x = q.front().xx ;
-    y = q.front().yy ;
+    Call_Mother = true ;
+    s = q.front().size ;
+    for (int i = 0 ; i < N ; i++) {
+        for (int j = 0 ; j < N ; j++) {
+            if (sea[i][j] < s) Call_Mother = false ;
+        }
+     }
     
 }
 
 void BFS() {
+    int nx, ny ;
+    if (q.size() == 0) {
+        Call_Mother = true ;
+        return ;
+    }
     x = q.front().xx ;
     y = q.front().yy ;
+    s = q.front().size ;
+    e = q.front().eat ;
+    c = q.front().clk ;
+    visited[y][x] = true ;
+    cout << "x = " << x <<" y = " << y << " size = " << s << " eat = " << e << " clk = " << c << endl ;
+    q.pop() ;
+    c += 1 ;
+    for (int i = 0 ; i < 4 ; i++){
+        nx = x + dx[i] ;
+        ny = y + dy[i] ;
+        if (nx < 0 || ny < 0 || nx >= N || ny >= N || sea[ny][nx] > s || visited[ny][nx] == true) continue ;
+        else {
+            visited[ny][nx] = true ;
+            if (sea[ny][nx] < s && sea[ny][nx] > 0) {
+                e += 1 ;
+                if (s == e) {
+                    s += 1 ;
+                    e = 0 ;
+                }
+                while( !q.empty() ) q.pop();
+                q.push({ny, nx, s, e, c}) ;
+                result = c ;
+                cout << "tmpresult = " << result << endl ;
+                sea[ny][nx] = 0 ;
+                cout << " -------find!!!-------" << endl ;
+                find_fish() ;
+                return ;
+            }
+            else {
+       //         cout << "(" << nx << "," << ny << ") 넣는다." << endl ;
+                q.push({ny, nx, s, e, c}) ;
+            }
+        }
+    }
+    BFS() ;
+}
+                       
+
+void solve() {
+    while(true) {
+        if (Call_Mother == true) {
+            return ;
+        }
+        else {
+            for (int i = 0 ; i < N ; i++) {
+                for (int j = 0 ; j < N ; j++) {
+                    visited[i][j] = false ;
+                }
+            }
+//            int a, b ;
+//            a = q.front().xx ;
+//            b = q.front().yy ;
+//            visited[b][a] = true ;
+            BFS() ;
+        }
+    }
+    
 }
 
 
@@ -43,11 +111,18 @@ int main() {
         for (int j = 0 ; j < N ; j++){
             cin >> sea[i][j] ;
             if (sea[i][j] == 9) {
-                q.push({i, j}) ;
+                q.push({i, j, 2, 0, 0}) ;
                 sea[i][j] = 0 ;
-                
             }
         }
     }
+    solve() ;
+//    for (int i = 0 ; i < N ; i++) {
+//        for (int j = 0 ; j < N ; j++) {
+//            cout << sea[i][j] << " ";
+//        }
+//        cout << endl ; 
+//    }
+    cout  << result ;
     return 0;
 }
---
+++ Baekjoon_Algorithm/Algorithm_cpp/카카오/15999_뒤집기/15999_뒤집기/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  15999_뒤집기
+//
+//  Created by 임태완 on 2019. 9. 5..
+//  Copyright © 2019년 임태완. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
